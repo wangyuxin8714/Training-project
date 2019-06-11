ef3f000006eebd2ce1e6b21471cd58a9cedc463f
wangyuxin8714 <48470286+wangyuxin8714@users.noreply.github.com>
2019-06-12 07:56:43 +0800

    Update index.js

--- manager/src/views/MainPage/index.js
+++ manager/src/views/MainPage/index.js
@@ -3,6 +3,32 @@ import style from "./index.css";
 import { Menu, Icon, Dropdown, Button } from "antd";
 const { SubMenu } = Menu;
 
+const menu = (
+  <Menu>
+    <Menu.Item>
+      <a target="_blank" rel="noopener noreferrer" href="http://www.alipay.com/">
+          个人中心
+      </a>
+    </Menu.Item>
+    <Menu.Item>
+      <a target="_blank" rel="noopener noreferrer" href="http://www.taobao.com/">
+        我的班级
+      </a>
+    </Menu.Item>
+    <Menu.Item>
+      <a target="_blank" rel="noopener noreferrer" href="http://www.tmall.com/">
+        设置
+      </a>
+    </Menu.Item>
+    <Menu.Item>
+      <a target="_blank" rel="noopener noreferrer" href="http://www.tmall.com/">
+          退出登录
+      </a>
+    </Menu.Item>
+  </Menu>
+);
+
+
 class MainPage extends Component {
   constructor(props) {
     super(props);
