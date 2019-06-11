ba7e9b2f8c2b71929c9f8337b3b6c364f3cbf475
wangyuxin8714 <48470286+wangyuxin8714@users.noreply.github.com>
2019-06-12 07:59:37 +0800

    Update router.js

--- manager/src/router.js
+++ manager/src/router.js
@@ -1,23 +1,19 @@
 import React from 'react';
 import { Router, Route, Switch  } from 'dva/router';
 import MainPage from "./views/MainPage/";
-import Login from "./views/login"
+// import Login from "./views/login"
 
-<<<<<<< HEAD
 import Login from './views/login/Login';
-=======
->>>>>>> srr
+
 
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
       <Switch>
         <Route path="/main" component={MainPage} />
-<<<<<<< HEAD
+
         <Route path="/" exact component={Login} />
-=======
-        <Route path="/" component={Login} />
->>>>>>> srr
+
       </Switch>
     </Router>
   );
