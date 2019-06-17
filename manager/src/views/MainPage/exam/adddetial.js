67e4bc28ac750241c7a63724a6c4926b2b4bb1ad
wyx <1510172244@qq.com>
2019-06-17 20:44:55 +0800

    班级管理

--- manager/src/views/MainPage/exam/adddetial.js
+++ manager/src/views/MainPage/exam/adddetial.js
@@ -1,66 +1,66 @@
-import React,{useEffect} from 'react';
-import { Form, Layout, Breadcrumb,message   } from "antd";
-import { connect } from 'dva';
+// import React,{useEffect} from 'react';
+// import { Form, Layout, Breadcrumb,message   } from "antd";
+// import { connect } from 'dva';
 
-const { Content } = Layout;
-function Addexam(props){
-    useEffect(()=>{
-        props.examType()
-        props.coursetype()
-    },[])
+// const { Content } = Layout;
+// function Addexam(props){
+//     useEffect(()=>{
+//         props.examType()
+//         props.coursetype()
+//     },[])
 
-    useEffect(()=>{
-        if(props.question.addexamcode===1){
-            message.success('添加考试试卷成功')
-        }
-    },[])
-    let submitexam = e => {
-        e.preventDefault();
-        props.form.validateFields((err, values) => {
-            if (!err) {
-                // console.log('Received values of form: ', values);
-                // props.addexam({
-                //     subject_id:values.coursetype,
-                //     exam_id:values.examtype,
-                //     title:values.username,
-                //     number:values.questionnum,
-                //     start_time:+values.examtime[0]._d,
-                //     end_time:+values.examtime[1]._d
-                // })
-            }
-        });
-    };
+//     useEffect(()=>{
+//         if(props.question.addexamcode===1){
+//             message.success('添加考试试卷成功')
+//         }
+//     },[])
+//     let submitexam = e => {
+//         e.preventDefault();
+//         props.form.validateFields((err, values) => {
+//             if (!err) {
+//                 // console.log('Received values of form: ', values);
+//                 // props.addexam({
+//                 //     subject_id:values.coursetype,
+//                 //     exam_id:values.examtype,
+//                 //     title:values.username,
+//                 //     number:values.questionnum,
+//                 //     start_time:+values.examtime[0]._d,
+//                 //     end_time:+values.examtime[1]._d
+//                 // })
+//             }
+//         });
+//     };
 
     // const { getFieldDecorator } = props.form;
-    return(
-        <Layout style={{ padding: 0}}>
-        {/* <div> */}
-            <Breadcrumb style={{ margin: "30px 0" }}>
-                <Breadcrumb.Item style={{fontSize:"20px"}}>创建试卷</Breadcrumb.Item>
-            </Breadcrumb>
-            <Content
-                style={{
-                    background: "#fff",
-                    padding: 24,
-                    margin: 0,
-                    height:"auto"
-                }} 
-            >
-                <h3></h3>
-            </Content>
-        </Layout>
-    )
-}
+//     return(
+//         <Layout style={{ padding: 0}}>
+//         {/* <div> */}
+//             <Breadcrumb style={{ margin: "30px 0" }}>
+//                 <Breadcrumb.Item style={{fontSize:"20px"}}>创建试卷</Breadcrumb.Item>
+//             </Breadcrumb>
+//             <Content
+//                 style={{
+//                     background: "#fff",
+//                     padding: 24,
+//                     margin: 0,
+//                     height:"auto"
+//                 }} 
+//             >
+//                 <h3></h3>
+//             </Content>
+//         </Layout>
+//     )
+// }
 
-const mapStateToProps = state=>{
-    return state
-}
+// const mapStateToProps = state=>{
+//     return state
+// }
   
-const mapDisaptchToProps = dispatch=>{
-    return {
+// const mapDisaptchToProps = dispatch=>{
+//     return {
         
-    }
-}
+//     }
+// }
 
 
-export default connect(mapStateToProps, mapDisaptchToProps)(Form.create()(Addexam))
+// export default connect(mapStateToProps, mapDisaptchToProps)(Form.create()(Addexam))
