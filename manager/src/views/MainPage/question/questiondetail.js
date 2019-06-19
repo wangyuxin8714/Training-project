e376da165fa053914028ff21759c0ba458aa3f1e
rong <1905960458@qq.com>
2019-06-19 21:15:50 +0800

    更改样式

--- manager/src/views/MainPage/question/questiondetail.js
+++ manager/src/views/MainPage/question/questiondetail.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Button, Select, Form, Input, Layout, Breadcrumb, Modal } from "antd";
 import { connect } from "dva";
 import Editor from "for-editor";
